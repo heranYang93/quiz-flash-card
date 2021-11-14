7cf56a6e1ff53e8347461151a378bab346d4fca4
hy <yang.hrn@gmail.com>
2021-11-14 23:15:58 +0000

    main features completed
    updated function getQUestion in row 125
    
    updated two functions interruptAndSHowScore and interruptAndBackToMain, integrated or disabled the reload function so that the app will run correctly.
    
    updated the time counter, it can now be interrupted correctly and interact with buttons
    
    added the following function showResult(), checkAnswer(), submitRecord(), displayHistory()

--- script.js
+++ script.js
@@ -10,55 +10,56 @@ const questionArray =[
     {
         Question:"question_2 [string]",
         Choices:['question_2 A [string]','question_2 B [string]','question_2 C [string]','question_2 D [string]'],
-        Answer:3
+        Answer:0
     },
 
     {
         Question:"question_3 [string]",
         Choices:['question_3 A [string]','question_3 B [string]','question_3 C [string]','question_3 D [string]'],
-        Answer:2
+        Answer:0
     },
 
     {
         Question:"question_4 [string]",
         Choices:['question_4 A [string]','question_4 B [string]','question_4 C [string]','question_4 D [string]'],
-        Answer:3
+        Answer:0
     },
 
     {
         Question:"question_5 [string]",
         Choices:['question_5 A [string]','question_5 B [string]','question_5 C [string]','question_5 D [string]'],
-        Answer:2
+        Answer:0
     },
 
     {
         Question:"question_6 [string]",
         Choices:['question_6 A [string]','question_6 B [string]','question_6 C [string]','question_6 D [string]'],
-        Answer:3
+        Answer:0
     }
 ]
-const previousResultPrompt = ['That is correct!','Wrong anwser!']   // Last answer prompt
-const totalTime=60          // Total time
-const timePenalty=-10       // Wrong answer time penalty
-const scorePenalty=-2       // Wrong answer score penalty
-const scoreGain=+5          // Correct answer score
+
+const previousResultPrompt = ['Correct!','Wrong!']   // Last answer prompt
+var timer = 30              // Total time (in second)
+const timePenalty = -10      // Wrong answer time penalty
+const scorePenalty = -2     // Wrong answer score penalty
+const scoreGain = +1        // Correct answer score
+const recordScoreToKeep = 5 // How many record do we want to keep when history is to be shown
 
 //  Buttons
-    //  Buttons to trigger events
+//  Buttons to trigger events
 const startQuizBtn = document.getElementById("startQuizBtn")
 const viewScoreBtn = document.getElementById("viewBoardBtn")
 const backToMainBtn = document.getElementById("backToMainBtn")
-
-//     //  Buttons for answers  (this to be created dynamically)
-// const answer0Btn = document.getElementById("#answerA")
-// const answer1Btn = document.getElementById("#answerB")
-// const answer2Btn = document.getElementById("#answerC")
-// const answer3Btn = document.getElementById("#answerD")
+const submitBtn = document.getElementById("submitName")
+const userInitialInput = document.getElementById("initialInput")
 
 // HTML elements whose contents need to be updated
 const questionNumberEl = document.querySelector(".questionNumber")              // This question number
 const questionContentEl = document.querySelector(".questionContent")            // This question content
 const residualTimeDisplayContent = document.querySelector(".residualTime")      // timer content to be updated
+const lastResultEl = document.querySelector(".lastResult")                      // how was the last question, was it correct or incorrect?
+const thisResultBlockEl = document.querySelector('.thisResultBlock')            // My last result
+const scoreHistoryEl = document.querySelector('.scoreHistory')                  // Score board list
 
 //  Blocks to be displayed in DIFFERENT stages of quiz
 const viewScoreBoardEl = document.querySelector('.viewScoreBoardBlock')
@@ -68,13 +69,12 @@ const countDownEl = document.querySelector(".countDownBlock")
 const previousResultEl = document.querySelector(".previousResultBlock")
 const questionBlockEl = document.querySelector(".questionBlock")
 const answerBlockEl = document.querySelector('.answerBlock')
-const thisResultBlockEl = document.querySelector('.thisResultBlock')
 const historyDisplayBlockEl = document.querySelector('.historyDisplayBlock')
 const backToMainEl = document.querySelector('.backToMainBlock')
 
 // Items to be displayed in different stages
 const allElementArr = [viewScoreBoardEl, quizIntroEl, startButtonEl, countDownEl, previousResultEl, questionBlockEl, answerBlockEl, thisResultBlockEl, historyDisplayBlockEl, backToMainEl]
-
+// Break down list of elements to display in different stage of the quiz
 const startDisplayElementArr = [viewScoreBoardEl, quizIntroEl, startButtonEl]
 const inQuizDisplayElementArr = [viewScoreBoardEl, countDownEl, previousResultEl, questionBlockEl, answerBlockEl, backToMainEl]
 const resultPageDisplayElementArr = [viewScoreBoardEl,thisResultBlockEl,backToMainEl]
@@ -86,28 +86,16 @@ const scoreBoardDisplayElementArr = [historyDisplayBlockEl,backToMainEl]
 var currentDisplayStage = 'start'
 //  Current question, which question is currently being displayed?
 var currentQuestionIndex = 0
-// Timer - how many seconds left
-var countDown = totalTime   
 // Last question result prompt - was the last question correct or not?
 var lastQuestionResult
 // Score
-var score
-// Result by question, 1 is correct, 0 is wrong, this list is to be used to display the last round of quiz
-var scoreHistory = []
-// Update the score to the local storage
-localStorage.setItem('scoreHistory',scoreHistory)
+var score = 0
+// Create a variable called recordList to append records
+var recordList = JSON.parse(localStorage.getItem('record'));
 //  What page is currently being displayed, it is
 var thisDisplay = startDisplayElementArr
-
-// Button functions
-// When the "Start Quiz" button is clicked, start the game
-startQuizBtn.addEventListener("click", startQuiz)
-// When the "View Score Board" button is clicked, start the game
-viewScoreBtn.addEventListener('click', interruptAndShowScore)
-// When the "Back to main" button is clicked, start the game
-backToMainBtn.addEventListener('click', interruptAndBackToMain)
-// When an answer button is clicked, 
-
+//  Previous result text area
+var previousResultText = document.querySelector('.previousResultText')
 
 function init(){
     for (i in allElementArr) {
@@ -149,6 +137,7 @@ function getQuestion(questionId){
     questionContentEl.innerHTML = qContent
     
     for (i in qChoicesArray){
+
         //create a list item li
         var thisAnswerListItem = document.createElement('li');
         // create a button within the list item button
@@ -159,31 +148,16 @@ function getQuestion(questionId){
         thisAnswerButton.setAttribute('data-answerKey',i)
         //asign a class called 'singleAnswerBtn' to each class
         thisAnswerButton.setAttribute('class','singleAnswerBtn')
+        //add event listener to each button, when each button is triggered, execute checkAnswer function
+        thisAnswerButton.addEventListener('click',checkAnswer)
         //put this answer button 
         thisAnswerListItem.append(thisAnswerButton)
         document.querySelector(".answerList").append(thisAnswerListItem)
+
     }
 }
 
-//timer function should be revised 
-function initTimer() {
-
-    countDown = totalTime
-
-    // Sets interval in a variable called timer its interval is set as 1000ms
-    var timer = setInterval(function() {
-
-        countDown--;
-        residualTimeDisplayContent.innerHTML = countDown;
-  
-        if (countDown == 0) {
-            clearInterval(timer)
-            showResult()
-        }
-    }, 1000);
-  }
-
-function startQuiz(event) {
+function startQuiz(event){
 
     event.preventDefault()
 
@@ -198,15 +172,150 @@ function startQuiz(event) {
     currentQuestionIndex=0
 
     //Get next question question
-    getQuestion(currentQuestionIndex)
+    getQuestion(currentQuestionIndex)    
 
     //Initiate timer
     initTimer()
 }
 
-function interruptAndShowScore (event) {
+function interruptAndShowScore(event){
+
+    //the key difference between this function and the displayHistory function is
+    // that this function has to prevent default 
+    event.preventDefault()
+
+    // Let timer know that the quiz is interrupted
+    timer = 'interrupted'
+
+    //Display history
+    displayHistory()
+}
+
+function interruptAndBackToMain(event){
 
     event.preventDefault()
+
+    // Let timer know that the quiz is interrupted
+    timer = 'interrupted'
+
+    //Disable the previous content in the start screen
+    disableEl(thisDisplay)
+
+    //Update the list of elements to be displayed and enable the new contents
+    thisDisplay=startDisplayElementArr
+    enableEl(thisDisplay)
+
+    window.location.reload();
+}
+
+function showResult(){
+
+    //Disable the previous content in the start screen
+    disableEl(thisDisplay)
+
+    //Update the list of elements to be displayed and enable the new contents
+    thisDisplay=resultPageDisplayElementArr
+    enableEl(thisDisplay)
+
+    //render this result
+    lastResultEl.innerHTML = score
+
+}
+
+function checkAnswer(event){
+
+    // prevent default
+    event.preventDefault()
+
+    // extract the answer from 
+    var selectedAnswer = parseInt((event.target.getAttribute("data-answerkey")))
+    
+    // when answer is correct
+    if (selectedAnswer === questionArray[currentQuestionIndex].Answer){
+        //update score
+        score += scoreGain
+        // update previous result strip (didn't want to use window.alert)
+        previousResultText.innerHTML = previousResultPrompt[0]
+    }
+    else{
+        //update score
+        score += scorePenalty
+        //update timer
+        timer += timePenalty
+        // update previous result strip (didn't want to use window.alert)
+        previousResultText.innerHTML = previousResultPrompt[1]
+    }
+
+    // update 
+    currentQuestionIndex += 1
+
+    // If this is the last question
+    if (currentQuestionIndex === questionArray.length){
+        showResult()
+    }
+    
+    // If this is not the last question
+    else {
+        getQuestion(currentQuestionIndex)
+    }
+
+}
+
+function initTimer(){
+    
+    var timerInterval = setInterval(function() {
+
+        timer--;
+
+        residualTimeDisplayContent.innerHTML = timer;
+        
+        //Once the timer runs out, either due to time loss or wrong answers, it will clear
+        if(timer <= 0) {
+            clearInterval(timerInterval);
+            showResult();
+        }
+
+        //When the game is interrupted by button
+        else if (timer ==='interrupted'){
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+} 
+
+function submitRecord(event){
+
+    event.preventDefault()
+
+    var thisName = userInitialInput.value.trim();
+
+    // create a variable for the most recent result
+    var lastRecord = {
+        // variable defined in row 52
+        thisName: thisName,
+        thisScore: parseInt(score)  //Make sure this is saved as an INTEGER
+    }
+
+    // What is the last history in the local storage?
+    // Row 91: var record = JSON.parse(localStorage.getItem('record'));
+    // If the recordList has NEVER BEEN CREATED, then create a blank list to append items 
+    if (!recordList){
+        recordList=[]
+    }
+    // append this result to the score history list
+    recordList.push(lastRecord);
+
+    // sort the history in a DECENDING MANNER
+    // max to min, comparing their "thisScore" attribute!!!
+    recordList.sort(function(min, max) {
+        return max.thisScore - min.thisScore;
+    });
+
+    // Only keep the last three records and update it
+    recordList.splice(3)
+
+    // update this last history to the local sotrage
+    localStorage.setItem('record', JSON.stringify(recordList))
 
     //Disable the previous content in the start screen
     disableEl(thisDisplay)
@@ -219,106 +328,36 @@ function interruptAndShowScore (event) {
     displayHistory()
 }
 
-function interruptAndBackToMain(event){
-
-    event.preventDefault()
+function displayHistory(){
 
     //Disable the previous content in the start screen
     disableEl(thisDisplay)
 
     //Update the list of elements to be displayed and enable the new contents
-    thisDisplay=startDisplayElementArr
+    thisDisplay = scoreBoardDisplayElementArr
     enableEl(thisDisplay)
-    
+
+    // If someone click the button without finishing one round of quiz, then we still need a blank list to make the next step work
+    if (!recordList) {
+        recordList = []
+    };
+
+    for (i in recordList) {
+        var singleRecordEntry = document.createElement('li');
+        singleRecordEntry.innerHTML = i + ' - ' + recordList[i].thisName + ' - ' + recordList[i].thisScore;
+        scoreHistoryEl.appendChild(singleRecordEntry)
+    }
+
 }
 
-function showResult(){
-    //Disable the previous content in the start screen
-    disableEl(thisDisplay)
-
-    //Update the list of elements to be displayed and enable the new contents
-    thisDisplay=resultPageDisplayElementArr
-    enableEl(thisDisplay)
-}
-
-function answerBtn(event){
-    
-}
+// Button functions
+// When the "Start Quiz" button is clicked, start the game
+startQuizBtn.addEventListener('click', startQuiz)
+// When the "View Score Board" button is clicked, start the game
+viewScoreBtn.addEventListener('click', interruptAndShowScore)
+// When the "Back to main" button is clicked, start the game
+backToMainBtn.addEventListener('click', interruptAndBackToMain)
+// submit button once we finish the quiz
+submitBtn.addEventListener('click', submitRecord)
 
 init()
-
-// //text to add
-// startQuizEl.addEventListener('click',function startGame(){
-//     //display
-//     startQuizEl.setAttribute('class','hide')
-//     startQuizEl.removeAttribute('class','hide')
-
-//     getQuestion()
-//     startTimer()
-    
-// }
-// )
-
-// function getQuestion(){
-//     questionNumberEl.textContent = 
-//     questionContentEl.textContent = questionArray[currentQuestionIndex].Question
-    
-//     var buttonAreaEl=document.createElement('button')
-//     buttonAreaEl.innerHTML=''
-    
-//     //var questionChoices=questionArray[currentQuestionIndex].Choices
-
-//     for (var i=0; i<questionArray[currentQuestionIndex].Choices.length;i++){
-//         //create a differnet vriabl
-//         var thisButton=document.createElement('button')
-//         thisButton.setAttribute('class','answer')
-//         thisButton.innerHTML=questionArray[currentQuestionIndex].Choices[i]
-//         buttonAreaEl.appendChild(thisButton)
-//         thisButton.onclick=checkAnser
-//     }
-// }
-
-// function checkAnswer(){
-//     if (this.textContent === questionArray[currentQuestionIndex].Answer){
-//         alert('correct')
-//         score+=1
-//         console.log('score',score)
-//     }
-//     else{
-//         alert('incorrect')
-//         incorrectScore+=1
-//         //time penalty
-        
-//         console.log('incorrectScore',incorrectScore)
-//     }
-
-//     currentQuestionIndex++
-
-//     if (currentQuestionIndex===questionArray.length){
-//         endGame()
-//     }
-//     else {
-//         getQuestion()
-//     }
-
-// }
-
-// function startTimer(){
-//     var timeInterval=setInterval(function(){
-//         time--
-//         timerEl.textContent='You have '+time+' seconds left'
-
-//         if (time===0){
-            
-//             timerEl.textContent='Game Ends'
-//             clearInterval(timeInterval)
-//             endGame()
-//         }
-
-//     }
-//     ,1000)
-
-//     if (this.textContent !== questionArray[currentQuestionIndex].Answer){
-//         time-=10
-//     }
-// }
